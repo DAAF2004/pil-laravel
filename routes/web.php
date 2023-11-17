0ef8e128f898edc5304b0a446d0e13842a5b122b
AxelMach <ditaauliaalfarid@gmail.com>
2023-11-17 17:16:51 +0700

    PUSH

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/mahasiswa', function () {
+    return view('mahasiswa');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
